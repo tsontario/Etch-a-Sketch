a2b8222e349b885a577823030e07695b22f66d59
Tim Smith <tsontario@gmail.com>
2016-12-15 10:17:54 -0500

    Hovering triggers grid color change

--- etch.js
+++ etch.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
 	for (var j=0; j<16; j++) {
 		for (var i=0; i<16; i++) {
 			$('<div />', {
@@ -7,7 +6,12 @@ $(document).ready(function() {
 			}).appendTo($('.grid-holder'));
 		}
 	}
+
+	$('.box').hover(function() {
+		$(this).css('background-color', '#AAA');
+	});
 });
+
 
 
 
