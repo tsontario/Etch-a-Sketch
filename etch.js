d03e2f39b4b1515ed6fcda686d4f3aaf104ebbf4
Tim Smith <tsontario@gmail.com>
2016-12-15 12:15:46 -0500

    Grid is resizeable. Borders are darker

--- etch.js
+++ etch.js
@@ -1,17 +1,48 @@
 $(document).ready(function() {
-	for (var j=0; j<16; j++) {
-		for (var i=0; i<16; i++) {
-			$('<div />', {
-				'class' : 'box'
-			}).appendTo($('.grid-holder'));
-		}
-	}
+	makeGrid(16);
 
-	$('.box').hover(function() {
-		$(this).css('background-color', '#AAA');
+	$(document).on('mouseenter', '.box', function() {
+			$(this).css('background-color', '#AAA');
+		});
+	// Keep grid size, reset colors
+	$('#reset').click(function() {
+		$('.box').css('background-color', '#FFF');
+	});
+
+	// New grid size:
+	$('#new').click(function() {
+		var val = prompt("Enter the grid size you want (max = 64).");
+		if ($.isNumeric(val) && val > 0 && val <= 64) {
+			$('.grid-holder').empty();
+			makeGrid(val);
+		}
+		else {
+			alert("Error. Please enter an integer.");
+		}
 	});
 });
 
 
+function makeGrid(size) {
+	for (var j=0; j<size; j++) {
+		$row = $('<tr />', {
+			'class' : 'row'
+		});
+		$row.appendTo($('.grid-holder'));
+		for (var i=0; i<size; i++) {
+			$('<td />', {
+				'class' : 'box'
+			}).appendTo($row);
+			console.log('looping');
+		}
+	}
+	$dimension = 480.0/(size) - 2;
+	$('.row').css('width', '480px');
+	$('.row').css('height', $dimension);
+	$('.box').css('height', $dimension);
+	$('.box').css('width', $dimension);
+	
+
+}
 
 
